d858b098a2c41a59809335568c99b9e6cdd4a0db
Dinhminhduc <72441314+Dinhminhduc@users.noreply.github.com>
2020-12-19 19:29:19 +0700

    ok

---
+++ js/Our-Offer/Main3.js
@@ -0,0 +1,17 @@
+function main3(){
+    return" <div class=\"container\">\n" +
+        "        <div class=\"boxduc\">\n" +
+        "            <div id=\"content1\">\n" +
+        "                <div class=\"fastfood\"><p>FAST FOOD</p></div>\n" +
+        "                <div class=\"madebusiness\"> <p>MADE FOR BUSINESS</p></div>\n" +
+        "                <div class=\"since\"> <p>Since it's a restaurant, bar, caffe & tavern wordPress theme,Fast Food is tailored for serving food\n" +
+        "                    and drink business like bar, cafeteria,coffee shop, dinner,d ining room, inn,drive-in, grill, pizzeria, fast-food place\n" +
+        "                    ,hotdog tand</p></div>\n" +
+        "                <div class=\"viewduc\"><p>VIEW CLASSIC MENU</p></div>\n" +
+        "            </div>\n" +
+        "            <div class=\"content2\">\n" +
+        "                <img src=\"img/inner-landing-slider_1.jpg\" height=\"520\" width=\"650\"/></div>\n" +
+        "        </div>\n" +
+        "    </div>"
+}
+document.getElementById("main3").innerHTML = main3();
